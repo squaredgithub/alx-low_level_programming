d6756ea009882405c6520bea46171899d47498b2
Your Name <njugunafrancis077@gmail.com>
2022-12-08 08:23:34 -0800

    prints , followed by new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
+include <stdio.h>
 
 /**
  *
  *  *main - Entry point
  *
- * puts-prints a string
- *
  *   *Return: Always 0 (Success)
  *
  *    */
-
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
-
+		return (0);
 }
