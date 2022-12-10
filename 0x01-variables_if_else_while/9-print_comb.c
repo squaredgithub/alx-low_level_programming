006bbc8aa16c7480a40db18ced2c6f07471b563c
Your Name <njugunafrancis077@gmail.com>
2022-12-10 08:56:46 -0800

    a program that prints all possible combinations of single-digit numbers ,separated by ,, followed by a space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,3 +6,17 @@
 int main(void)
 {
 	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
