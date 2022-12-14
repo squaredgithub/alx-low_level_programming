7425d92dcb99b6e4438e234f15e977f347476f5b
Your Name <njugunafrancis077@gmail.com>
2022-12-14 09:29:16 -0800

    print putchar on a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucesss)
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
