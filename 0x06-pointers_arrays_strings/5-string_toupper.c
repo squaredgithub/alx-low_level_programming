9a2cc177b9829df2a19f4357a6c9de58311d16a6
squaredgithub <njugunafrancis077@gmail.com>
2023-01-08 01:13:56 -0800

    changes all lowercase letters of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - convert to uppercase
+ * Return: void
+ * @s: string
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = 'A' + (s[i] - 'a');
+		}
+		i++;
+	}
+	return (s);
+}
