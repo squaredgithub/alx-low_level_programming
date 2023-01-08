92f1c994f6bc0a1ec5751cd6f5b40904d0b50d4e
squaredgithub <njugunafrancis077@gmail.com>
2023-01-08 01:01:55 -0800

    reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - concatenates two strings
+ * @a: array
+ * @n: number of elements in arr
+ * Return: void
+*/
+void reverse_array(int *a, int n)
+{
+	int c2 = 0;
+
+	while (c2 < n)
+	{
+		int t1 = a[c2];
+		int t2 = a[n - 1];
+		a[c2] = t2;
+		a[n - 1] = t1;
+		c2++;
+		n--;
+	}
+}
