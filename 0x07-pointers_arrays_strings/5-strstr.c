9d0f834fa6a897574630ab28196cb09218c2393d
squaredgithub <njugunafrancis077@gmail.com>
2023-01-10 06:49:27 -0800

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: char array
+ * @needle: char array (keyword)
+ * Return: array
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *duplicate = haystack;
+
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+		{
+			needle++;
+			haystack++;
+		}
+		if (!*needle != '\0')
+		{
+			return (duplicate);
+		}
+		haystack++;
+	}
+	return (0);
+}
