83fb415192ef37de3a15174ae1c13132e25e1304
squaredgithub <njugunafrancis077@gmail.com>
2023-01-19 08:50:26 -0800

    searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array of integers
+ * @size: size of given array
+ * @cmp: pointer to the function to be used to compare values
+ *
+ * Return: index of the first element for which the cmp function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
